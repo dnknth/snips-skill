cb70db5c047045e73969906426b04ea6bc4306b0
dnknth <dk+git@krachbumm.de>
2021-01-12 21:27:52 +0100

    Add action template

---
+++ action-template/README.md
@@ -0,0 +1,3 @@
+# An example skill
+
+This is a [snips](https://snips.ai/) skill template based on [snips-skill](https://github.com/dnknth/snips-skill).
---
+++ action-template/action-example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from snips_skill import *
+from . i18n import *
+
+
+class ExampleSkill( Skill):
+    
+    'Your skill goes here'
+
+    def add_arguments( self):
+        super().add_arguments()
+        
+        self.parser.add_argument( '--option',
+            help='Optionally set an option')
+
+    @intent( 'example:intent')
+    def intent_handler( self, userdata, msg):
+        return _('Not implemented')
+
+
+if __name__ == '__main__':
+    ExampleSkill().run()
